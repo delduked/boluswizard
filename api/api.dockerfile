492eedecc8ee148bac1d531e1f016f81f00522cd
nate <nate.delduca@mintymint.info>
2022-11-23 21:26:24 -0500

    testing new docker image

--- api/api.dockerfile
+++ api/api.dockerfile
@@ -1,7 +1,7 @@
 FROM golang:latest AS stage1
 WORKDIR /app
 COPY . /app
-RUN go get
+RUN go install
 RUN go build -o /app/main
 
 FROM alpine:latest
--- api/types/status.go
+++ api/types/status.go
@@ -3,5 +3,5 @@ package types
 type Response struct {
 	Status int `json:"Status" redis:"Status"`
 	Error  any `json:"Error" redis:"Error"`
-	Data   any
+	Data   any `json:"-"`
 }
