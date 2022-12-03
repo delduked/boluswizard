bff3b5fb6214e87d73e7a7ab5d1857d706eff487
nate <nate@nated.ca>
2022-12-03 17:13:33 -0500

    setting up deployment for both dev and prod

--- ui/assets/loginpage/index.js
+++ ui/assets/loginpage/index.js
@@ -30,8 +30,8 @@ class LoginPage {
                 // assign token to local storage
                 const responseJson = await res.json();
                 console.log(responseJson.Data);
-                //setTimeout(() => { document.cookie = document.cookie + `authToken=${responseJson.Data.Token};path=/wizard`; }, 500);
-                //setTimeout(() => { document.cookie = document.cookie + `authToken=${responseJson.Data.Token};path=/u/${responseJson.Data.Username}`; }, 1000);
+                setTimeout(() => { document.cookie = document.cookie + `wizardToken=${responseJson.Data.Token};path=/wizard`; }, 500);
+                setTimeout(() => { document.cookie = document.cookie + `authToken=${responseJson.Data.Token};path=/u/${responseJson.Data.Username}`; }, 1000);
                 this.updateMessage("Login successful!");
                 setTimeout(() => { document.location.href = `/u/${responseJson.Data.Username}/home`; }, 1500);
             }
